cda4f0c132ff6f5799a84532d6ba690d74c0f511
didi2612 <github@azmiproductions.com>
2024-10-30 16:59:09 +0800

    added arduino code

---
+++ Week 4/Code/4a.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+    mpu.initialize();
+    
+    if (mpu.testConnection()) {
+        Serial.println("MPU6050 connected successfully.");
+    } else {
+        Serial.println("MPU6050 connection failed.");
+    }
+}
+
+void loop() {
+    int16_t ax, ay, az, gx, gy, gz;
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    
+    Serial.print("AX: "); Serial.print(ax);
+    Serial.print(" AY: "); Serial.print(ay);
+    Serial.print(" AZ: "); Serial.print(az);
+    Serial.print(" GX: "); Serial.print(gx);
+    Serial.print(" GY: "); Serial.print(gy);
+    Serial.print(" GZ: "); Serial.println(gz);
+
+    delay(100); 
+}
